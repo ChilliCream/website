02db792023944242db64cb1d9ab726e701e64e16
Rafael Staib <me@rafaelstaib.com>
2018-09-06 10:25:48 +0200

    Moved blog articles from hotchocolate to the main website (#4)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -7,8 +7,10 @@
 
 // See https://docusaurus.io/docs/site-config.html for all the possible
 
+const title = "ChilliCream";
+
 const siteConfig = {
-  title: "ChilliCream",
+  title,
   tagline: "We build hot & smooth software",
   url: "https://chillicream.com",
   cname: "chillicream.com",
@@ -29,7 +31,7 @@ const siteConfig = {
     secondaryColor: "#432a19"
   },
   stylesheets: ["https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:700,400"],
-  copyright: "Copyright © " + new Date().getFullYear() + " ChilliCream",
+  copyright: `Copyright © ${new Date().getFullYear()} ${title}`,
   editUrl: "https://github.com/ChilliCream/website/edit/master/docs/",
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
